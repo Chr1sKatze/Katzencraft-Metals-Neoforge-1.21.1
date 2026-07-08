90443f1d61abdd676ac791319cdcbe21585d342e
nanik <nanik.post@gmail.com>
2026-07-09 00:04:09 +0200

    more reciped

--- src/main/java/net/chriskatze/katzencraftmetals/datagen/ModFoundryRecipeProvider.java
+++ src/main/java/net/chriskatze/katzencraftmetals/datagen/ModFoundryRecipeProvider.java
@@ -7,7 +7,6 @@ import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ItemLike;
 
 /**
  * Foundry-only recipe definitions.
@@ -23,12 +22,145 @@ public final class ModFoundryRecipeProvider {
     public static void build(
             RecipeOutput recipeOutput
     ) {
+        buildIronMeltingRecipes(
+                recipeOutput
+        );
+
+        buildCopperMeltingRecipes(
+                recipeOutput
+        );
+
         buildGoldMeltingRecipes(
                 recipeOutput
         );
 
         buildAlloyRecipes(
                 recipeOutput
+        );
+    }
+
+    private static void buildIronMeltingRecipes(
+            RecipeOutput recipeOutput
+    ) {
+        foundryMelting(
+                recipeOutput,
+                Items.RAW_IRON,
+                ModMoltenMetals.IRON.id(),
+                6,
+                20,
+                "raw_iron"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.IRON_INGOT,
+                ModMoltenMetals.IRON.id(),
+                6,
+                20,
+                "iron_ingot"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.IRON_NUGGET,
+                ModMoltenMetals.IRON.id(),
+                1,
+                10,
+                "iron_nugget"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.RAW_IRON_BLOCK,
+                ModMoltenMetals.IRON.id(),
+                54,
+                120,
+                "raw_iron_block"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.IRON_BLOCK,
+                ModMoltenMetals.IRON.id(),
+                54,
+                120,
+                "iron_block"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.IRON_ORE,
+                ModMoltenMetals.IRON.id(),
+                6,
+                20,
+                "iron_ore"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.DEEPSLATE_IRON_ORE,
+                ModMoltenMetals.IRON.id(),
+                6,
+                20,
+                "deepslate_iron_ore"
+        );
+    }
+
+    private static void buildCopperMeltingRecipes(
+            RecipeOutput recipeOutput
+    ) {
+        foundryMelting(
+                recipeOutput,
+                Items.RAW_COPPER,
+                ModMoltenMetals.COPPER.id(),
+                6,
+                20,
+                "raw_copper"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.COPPER_INGOT,
+                ModMoltenMetals.COPPER.id(),
+                6,
+                20,
+                "copper_ingot"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.RAW_COPPER_BLOCK,
+                ModMoltenMetals.COPPER.id(),
+                54,
+                120,
+                "raw_copper_block"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.COPPER_BLOCK,
+                ModMoltenMetals.COPPER.id(),
+                54,
+                120,
+                "copper_block"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.COPPER_ORE,
+                ModMoltenMetals.COPPER.id(),
+                6,
+                20,
+                "copper_ore"
+        );
+
+        foundryMelting(
+                recipeOutput,
+                Items.DEEPSLATE_COPPER_ORE,
+                ModMoltenMetals.COPPER.id(),
+                6,
+                20,
+                "deepslate_copper_ore"
         );
     }
 
@@ -84,6 +216,37 @@ public final class ModFoundryRecipeProvider {
     private static void buildAlloyRecipes(
             RecipeOutput recipeOutput
     ) {
+        /*
+         * Temporary Foundry progression:
+         *
+         * Steel is currently a refinement-style alloy recipe because the alloy
+         * system only accepts molten metal ingredients. We can add carbon/coal
+         * support later if we want proper iron + carbon steel.
+         */
+        FoundryAlloyRecipeBuilder.alloy(
+                        ModMoltenMetals.STEEL.id(),
+                        6
+                )
+                .ingredient(
+                        ModMoltenMetals.IRON.id(),
+                        6
+                )
+                .processingTime(
+                        100
+                )
+                .experience(
+                        1
+                )
+                .requiredTier(
+                        1
+                )
+                .save(
+                        recipeOutput,
+                        foundryAlloyId(
+                                "steel"
+                        )
+                );
+
         FoundryAlloyRecipeBuilder.alloy(
                         ModMoltenMetals.PLATINUM.id(),
                         6
@@ -109,6 +272,34 @@ public final class ModFoundryRecipeProvider {
                         recipeOutput,
                         foundryAlloyId(
                                 "platinum"
+                        )
+                );
+
+        FoundryAlloyRecipeBuilder.alloy(
+                        ModMoltenMetals.MYTHRIL.id(),
+                        6
+                )
+                .ingredient(
+                        ModMoltenMetals.STEEL.id(),
+                        3
+                )
+                .ingredient(
+                        ModMoltenMetals.PLATINUM.id(),
+                        3
+                )
+                .processingTime(
+                        140
+                )
+                .experience(
+                        3
+                )
+                .requiredTier(
+                        2
+                )
+                .save(
+                        recipeOutput,
+                        foundryAlloyId(
+                                "mythril"
                         )
                 );
     }
